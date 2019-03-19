612d40ef985a566b77a7566d3866f744faee1184
Gustavo Collaço <37025851+gucollaco@users.noreply.github.com>
2019-03-19 15:18:19 -0300

    Almost ok

--- shell.c
+++ shell.c
@@ -6,8 +6,14 @@
 
 // struct to store the commands
 typedef struct commands {
-   char **com;
+	char **com;
+	pid_t child;
 } commands;
+
+// struct to store the file descriptors
+typedef struct all_pipes {
+   	int pipe_ends[2];
+} all_pipes;
 
 int *get_pipe_pos(int argc, char **argv) {
 	// store on the array out, the indexes (of the args) in which the pipe command occurred
@@ -70,6 +76,7 @@ int main(int argc, char **argv) {
 	//printf("quantity %d\ns", *quantity);
 	//printf("value %s\n", argv[14]);
 
+	// separating the commands
 	int n = 0;
 	int used = 0;
 	for(int c = 0; c < *quantity; c++) {
@@ -84,10 +91,10 @@ int main(int argc, char **argv) {
 			used += *quantity-1;
 
 			int remaining = (argc-1)-used;
-			/*printf("-----command %d-----\n", c+1);
+			printf("-----command %d-----\n", c+1);
 			printf("(c/index: %d - n/beginning index: %d)\n", c, n);
 			printf("(remaining arguments: %d - used/already included: %d)\n", remaining, used);
-			print_command(remaining, coms[c].com);*/
+			print_command(remaining, coms[c].com);
 		} else {
 			// while last command still not reached
 			// keep settings the commands properly, putting '|' as null
@@ -96,14 +103,98 @@ int main(int argc, char **argv) {
 
 			coms[c].com = &command[n];
 			coms[c].com[pos[c+1]-n] = NULL;
-			/*printf("-----command %d-----\n", c+1);
+			printf("-----command %d-----\n", c+1);
 			printf("(c/index: %d - n/beginning index: %d - null/nullify: %d)\n", c, n, pos[c+1]-n);
-			print_command(pos[c+1]-n, coms[c].com);*/
+			print_command(pos[c+1]-n, coms[c].com);
 
 			used += pos[c+1]-n;
 			n = pos[c+1]+1;
 		}
 	}
+
+	// creating pipe ends
+	struct all_pipes pipes[*quantity-1];
+	for(int i = 0; i < *quantity-1; i++) {
+		if(pipe(pipes[i].pipe_ends) < 0) {
+			perror("pipe");
+			return -1;
+		}
+	}
+
+	printf("*******************************\n");
+	// redirecting the commands
+	pid_t* pids = (pid_t*) malloc(*quantity * sizeof(pid_t));
+	int i;
+	for(i = 0; i < *quantity; i++) {
+		printf("** (parent - %d) command (%d - %s)\n", getpid(), i,  coms[i].com[0]);
+		//coms[i].child = fork();
+        pids[i] = fork();
+		if(pids[i] < 0) {
+			perror("fork child");
+			return -1;
+		}
+		if(pids[i] == 0) {
+			if(i == 0) { // first command
+				//close(pipes[i].pipe_ends[0]);
+				//close(STDOUT_FILENO);
+				printf("^^ (first child - %d) command %d\n", getpid(), i);
+				close(pipes[i].pipe_ends[0]);
+				dup2(pipes[i].pipe_ends[1], STDOUT_FILENO);
+			}
+			if(i == *quantity-1) {//*quantity-1) { // last command
+				printf("^^ (last child - %d) command (last) %d\n", getpid(), i);
+				//close(pipes[i-1].pipe_ends[1]);
+				dup2(pipes[i-1].pipe_ends[0], STDIN_FILENO);
+				close(pipes[i-1].pipe_ends[0]);
+			} else { // middle command
+				printf("^^ (mid child - %d) command %d\n", getpid(), i);
+				//close(pipes[i-1].pipe_ends[1]);
+				dup2(pipes[i-1].pipe_ends[0], STDIN_FILENO);
+				close(pipes[i-1].pipe_ends[0]);
+				//close(pipes[i-1].pipe_ends[0]);
+				//close(pipes[i].pipe_ends[0]);
+				dup2(pipes[i].pipe_ends[1], STDOUT_FILENO);
+				close(pipes[i].pipe_ends[1]);
+				//close(pipes[i].pipe_ends[1]);
+			}
+
+			execvp(coms[i].com[0], coms[i].com);
+
+			/*for(int i2=0; i2 < (*quantity-1); i++) {
+                close(pipes[i2].pipe_ends[0]);
+                close(pipes[i2].pipe_ends[1]);
+            }*/
+			
+			/*printf("-> filho (%d) %d\n", getpid(), i);
+			if(i > 0) {
+				if(dup2(all[(i-1)*2], STDIN_FILENO)) {
+				
+                    perror(" dup22");///j-2 0 j+1 1
+                    exit(EXIT_FAILURE);
+				}
+			}
+			if(i != *quantity-1) {
+				if(dup2(all[i*2+1], STDOUT_FILENO)) {
+				
+                    perror(" dup223");///j-2 0 j+1 1
+                    exit(EXIT_FAILURE);
+				}
+			}*/
+			/*for(int i2=0; i2 < (*quantity-1); i++) {
+                close(pipes[i2].pipe_ends[0]);
+                close(pipes[i2].pipe_ends[1]);
+            }*/
+			
+			//printf("-- filho (%d) %s %d\n", getpid(), coms[i].com[0], i);
+			//execvp(coms[i].com[0], coms[i].com);
+		} else {
+			printf("** (parent - %d) waiting child (%d)\n", getpid(), pids[i]);
+			waitpid(pids[i], NULL, 0);
+		}
+	}
+	//for(int i = 0; i < *quantity; i++) {
+	//	execvp(coms[i].com[0], coms[i].com);
+	//}
 
 	/*
 
@@ -147,3 +238,67 @@ int main(int argc, char **argv) {
 	return 0;
 
 }
+
+
+
+/*
+
+
+	com1 = &command[0];
+	com1[n] = NULL;
+	com2 = &command[n+1];
+	
+	/*
+	printf("-----com1:\n");
+	print_command(n, com1);
+	printf("-----com2:\n");
+	print_command((argc-1)-(n+1), com2);
+	
+
+	int fd[2];
+	if(pipe(fd) < 0) {
+		perror("pipe:");
+		return -1;
+	}
+
+	pid_t filho1, filho2;
+
+	filho1 = fork();
+	
+	if(filho1 < 0) { //testar se fork foi bem sucedido
+		perror("fork filho1:");
+		return -1;
+	}
+	if(filho1 == 0) { //filho1 executa comando 1
+		printf("-> sou filho (%d) -> pai: %d\n", getpid(), getppid());
+		//close(fd[0]);
+		//close(STDOUT_FILENO);
+		dup2(fd[1], STDOUT_FILENO); // duplica entrada (escrita) do pipe na saída
+		//close(fd[1]);
+		execvp(com1[0], com1);
+	} else { //pai shell aguarda filho1
+		printf("-> sou pai (%d) -> filho: %d\n", getpid(), filho1);
+		waitpid(filho1, NULL, 0);
+		printf("-> sou pai (%d) -> filho (%d) acabou\n", getpid(), filho1);
+	} //quando filho1 termina, seu resultado já está no pipe para o filho2
+
+	
+	//filho 2 vai ler do índice 0
+	filho2 = fork();
+
+	if(filho2 < 0) {
+		perror("fork filho2:");
+		return -1;
+	}
+	if(filho2 == 0) {
+		printf("-> sou filho (%d) -> pai: %d\n", getpid(), getppid());
+		dup2(fd[0], STDIN_FILENO);
+		execvp(com2[0], com2); //entrada padrão vai ser carregada com a saída do filho 1
+	} else {
+		printf("-> sou pai (%d) -> filho: %d\n", getpid(), filho2);
+		waitpid(filho2, NULL, 0);
+		printf("-> sou pai (%d) -> filho (%d) acabou\n", getpid(), filho2);
+		//printf("Pai 2\n");
+	}
+
+	return 0;*/
